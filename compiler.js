fd60a66a71527db98117c9d4479ffeed7b850d18
needleful <needleful@outlook.com>
2026-08-15 06:29:01 -0700

    Parsing simple conditions

--- compiler.js
+++ compiler.js
@@ -24,7 +24,25 @@ const Tok = {
 	markItalics: 21,
 	markBold: 22,
 	markStrike: 22,
+	markInterpolate:23,
+
+	exStart: 30,
+	exEnd: 31,
+	exIdentifier: 32,
+	exStartOp: 33,
+	exEndOp: 34,
+	exArgSplit: 35,
+	exText: 36,
+	exRawValue: 37,
+	exDynamicVar: 38,
+	exEscape: 40
 };
+
+let TokNames = {};
+for(let k in Tok) {
+	TokNames[Tok[k]] = k;
+}
+Object.freeze(TokNames);
 
 // Creates an array of Token (prefix: tk)
 function tokenize(strText) {
@@ -40,19 +58,26 @@ function tokenize(strText) {
 			intStart: intStart,
 			intLength: intLength
 		});
-		intC += intLength;
+		intC = intStart + intLength;
 	}
 	function pushTextTk(tokType, strText) {
 		pushTk(tokType, strText.length);
 	}
-	function pushError(strDescription, strText) {
+	function pushErrorTk(strDescription, strText, intStart = -1) {
+		var intLength = strText.length;
+		if(intStart < 0) {
+			intStart = intC;
+		}
+		else {
+			intLength = intC - intStart;
+		}
 		a_tkResult.push({
 			tokType:Tok.invalid,
 			varInfo: strDescription,
-			intStart: intC,
-			intLength: strText.length
+			intStart: intStart,
+			intLength: intLength
 		});
-		intC += strText.length;
+		intC += intLength;
 	}
 	function isGood() {
 		return intC < strText.length;
@@ -80,7 +105,7 @@ function tokenize(strText) {
 			return false;
 		}
 	}
-	function matchesStrings(tokType, a_strMatches) {
+	function matchesOneOf(tokType, a_strMatches) {
 		if(!isGood()) {
 			return false;
 		}
@@ -105,12 +130,73 @@ function tokenize(strText) {
 			return false;
 		}
 	}
+	function tokenizeExpression() {
+		function grabIdentifer() {
+			return matchesRegex(Tok.exIdentifier, /^\p{Alpha}[\p{Alpha}\d_]*\b/u);
+		}
+		function grabDynVar() {
+			return matchesRegex(Tok.exDynamicVar, /^\$\p{Alpha}[\-'+\p{Alpha}\d_]*\b/u)
+		}
+		function grabRawValue() {
+			return matchesRegex(Tok.exRawValue, /^\#[^\]\|]/);
+		}
+		var intStart = intC;
+		// Optional starting operator
+		matchesString(Tok.exStartOp, '!');
+		// Required starting identifier or sub-expression
+		var bValid = (matchesString(Tok.exStart, '[')
+			|| grabIdentifer()
+			|| grabDynVar()
+			|| grabRawValue()
+		);
+		if(!bValid) {
+			if(matchesString(Tok.exStart, '[')) {
+				tokenizeExpression();
+			}
+			else {
+				pushErrorTk('Could not parse expression: no starting identifier', '', intStart);
+			}
+		}
+		// Function head
+		while(isGood()) {
+			if(grabIdentifer()) continue;
+			if(matchesRegex(Tok.exEndOp, /^[+\-=/\\|?<>&%$!@:]+/))
+				break;
+			if(matchesString(Tok.exEnd, ']')) {
+				return;
+			}
+			if(matchesString(Tok.exStart, '[')) {
+				tokenizeExpression();
+			}
+			matchesRegex(Tok.invalid, /^./);
+		}
+		// Function arguments
+		while(isGood()) {
+			if(matchesString(Tok.exArgSplit, '|')
+				|| grabDynVar()
+				|| grabRawValue()
+				|| matchesRegex(Tok.exText, /^[^\]\\|]/)
+			) {
+				continue;
+			}
+			if(matchesString(Tok.exStart, '[')) {
+				tokenizeExpression();
+				continue;
+			}
+			if(matchesString(Tok.exEnd, ']')) {
+				return;
+			}
+			matchesRegex(Tok.invalid, /^./);
+		}
+	}
 
-	// Some symbols are different at the start of a line.
-	var newLine = true;
+	// 0: start of line (allows narration, reply, and speaker markings. Checks for indentation)
+	// 1: control flow (allows conditions)
+	// 2: text and interpolation only
+	var intState = 0;
 	var indentation = 0;
 	while(isGood()){
-		if(newLine) {
+		if(intState < 1) {
 			var intSkipped = skipWhiteSpace();
 			if(intSkipped > indentation) {
 				pushTk(Tok.indent, intSkipped, intC - intSkipped);
@@ -121,29 +207,41 @@ function tokenize(strText) {
 			indentation = intSkipped;
 
 			if(matchesString(Tok.symNarration, '*')
-				|| matchesString(Tok.symSpeaker, '--')
+				|| matchesRegex(Tok.symSpeaker, /^[^\s-[\]]+\s*--/)
 				|| matchesRegex(Tok.comment, /^\/\/[^\n]*/)
 			) {
-				newLine = false;
+				intState = 1;
 				continue;
 			}
 		}
-		if(matchesString(Tok.markEscape, '|')) {
+		if(intState < 2) {
+			if(matchesString(Tok.exStart, '[')) {
+				tokenizeExpression();
+				continue;
+			}
+		}
+		if(matchesString(Tok.markEscape, '\\')) {
 			if(isGood()) { 
 				pushTk(Tok.textPlain, 1);
 			}
 			continue;
 		}
+		if(matchesString(Tok.markInterpolate, '#[')) {
+			tokenizeExpression();
+			intState = 2;
+			continue;
+		}
 		if(matchesString(Tok.markItalics, '/')
 			|| matchesString(Tok.markBold, '_')
 			|| matchesString(Tok.markStrike, '~')
-			|| matchesRegex(Tok.textPlain, /^[^\n\/_|]+/)
+			|| matchesString(Tok.textPlain, '#')
+			|| matchesRegex(Tok.textPlain, /^[^\n\/\\_#]+/)
 		) {
-			newLine = false;
+			intState = 2;
 			continue;
 		}
 		if(matchesRegex(Tok.newLine, /^\n+/)) {
-			newLine = true;
+			intState = 0;
 			continue;
 		}
 	}
@@ -179,6 +277,65 @@ function parse(strText, a_tkTokens) {
 		intIndent: 0,
 		intLine: -1
 	};
+	function parseExpression() {
+		var exResult = {
+			a_head: [],
+			a_tail: [],
+			strStartOp: undefined,
+			strEndOp: undefined,
+		}
+		var head = true;
+		// Get the head
+		while(isGood() && head) {
+			var tk = pop();
+			switch(tk.tokType) {
+			case Tok.exIdentifier:
+				exResult.a_head.push(tkText(tk));
+				break;
+			case Tok.exDynamicVar:
+				exResult.a_head.push({strVar: tkText(tk)});
+				break;
+			case Tok.exRawValue:
+				exResult.a_head.push({strRaw: tkText(tk)});
+				break;
+			case Tok.exEnd:
+				return exResult;
+			case Tok.exStart:
+				exResult.a_head.push(parseExpression());
+				break;
+			case Tok.exEndOp:
+				exResult.strEndOp = tkText(tk);
+				head = false;
+				break;
+			default:
+				head = false;
+				break;
+			}
+		}
+		while(isGood()) {
+			var tk = pop();
+			switch(tk.tokType) {
+			case Tok.exIdentifier:
+				exResult.a_tail.push(tkText(tk));
+				break;
+			case Tok.exDynamicVar:
+				exResult.a_tail.push({strVar: tkText(tk)});
+				break;
+			case Tok.exRawValue:
+				exResult.a_tail.push({strRaw: tkText(tk)});
+				break;
+			case Tok.exEnd:
+				return exResult;
+			case Tok.exStart:
+				exResult.a_tail.push(parseExpression());
+				break;
+			default:
+				console.error('Invalid argument: ', tkText(tk), tk)
+				return exResult;
+			}
+		}
+		return exResult;
+	}
 	var psParent = psRoot;
 	var intLine = 0;
 	var indent = 0;
@@ -227,17 +384,22 @@ function parse(strText, a_tkTokens) {
 			case Tok.unindent:
 				item.intIndent = tkNext.intLength;
 				break;
+			case Tok.symSpeaker:
+				var s = tkText(tkNext);
+				var l = s.length - 2;
+				item.strSpeaker = s.substr(0, l).trim();
+				break;
+			case Tok.exStart:
+				item.a_conditions.push(parseExpression());
+				break;
 			}
 		}
-		console.log('Indentation: ', item.a_varText, item.intIndent);
+		//console.log('Indentation: ', item.a_varText, item.intIndent);
 		if(item.a_varText.length || item.a_conditions.length){
 			if(item.intIndent > indent) {
 				var l = psParent.a_psChildren.length - 1;
 				if(l >= 0) {
 					psParent = psParent.a_psChildren[l];
-				}
-				else {
-					console.error('Duplicate indentation?');
 				}
 			}
 			else while(item.intIndent <= psParent.intIndent && psParent.psParent) {
@@ -261,12 +423,14 @@ function flatten(psParse) {
 		var intPrev = -1;
 		for(let i = 0; i < psParse.a_psChildren.length; i++) {
 			var psChild = psParse.a_psChildren[i];
+			psChild.strSpeaker = psChild.strSpeaker || psParse.strSpeaker;
 			var diaNode = {
 				intNext:-1,
 				intParent: psParse.intLine,
 				intChild:-1,
 				a_varText: psChild.a_varText,
 				a_conditions: psChild.a_conditions,
+				strSpeaker: psChild.strSpeaker
 			};
 
 			dc_int_dialog[psChild.intLine] = diaNode;
